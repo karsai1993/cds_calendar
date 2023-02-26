a2571f701c501b866151e20569532cab43dfdcdd
Karsai, Laszlo <karsai1993@gmail.com>
2023-02-26 21:14:22 +0100

    Removed unused code

--- cds.php
+++ cds.php
@@ -6,13 +6,6 @@
 * Author: CDS Admin
 **/
 
-/*
-* Questions:
-* 1. Should we display the length of an event? E.g.: 1 hour, 2 days, etc
-*/
-
-// TODO: Improve click behaviour!
-
 require 'cds_css.php';
 require 'cds_utils.php';
 require 'cds_functions.php';
@@ -97,49 +90,6 @@ function loadEvents($calendarId, $apiKey, $queryString) {
 
 	return $decodedResult;
 }
-
-/*
-TODO: Fix when monthly view should be created
-function identifyDateYearAndMonth($queryParamsDateYear, $queryParamsDateMonth) {
-    $defaultDateYearMin = date("Y");
-    $defaultDateMonthMin = date("m");
-
-    $year = $defaultDateYearMin;
-    $isYearGreater = (int) $queryParamsDateYear > (int) $defaultDateYearMin;
-    if (!is_null($queryParamsDateYear) && $queryParamsDateYear == '' && $isYearGreater) {
-        $year = $queryParamsDateYear;
-    }
-
-    $month = $defaultDateMonthMin;
-    if (!is_null($queryParamsDateMonth) && $queryParamsDateMonth == '' && ($isYearGreater || (int) $queryParamsDateMonth >= (int) $defaultDateMonthMin)) {
-        $month = $queryParamsDateMonth;
-    }
-
-    return $year.'-'.$month;
-}
-
-function identifyTimeMin($queryParamsDateYear, $queryParamsDateMonth) {
-    $dateYearAndMonth = identifyDateYearAndMonth($queryParamsDateYear, $queryParamsDateMonth);
-
-    $dateTimeMinCalculated = ''.$dateYearAndMonth.'-01';
-    $dateTimeMinAbsolut = date('Y-m-d');
-
-    if (strtotime($dateTimeMinAbsolut) > strtotime($dateTimeMinCalculated)) {
-        $dateTime = new DateTime($dateTimeMinAbsolut);
-        return $dateTime->format('Y-m-d\T\0\0:\0\0:\0\0\Z');
-    } else {
-        $dateTime = new DateTime($dateTimeMinCalculated);
-        return $dateTime->format('Y-m-01\T\0\0:\0\0:\0\0\Z');
-    }
-
-}
-
-function identifyTimeMax($queryParamsDate, $defaultTimeMin) {
-    $dateYearAndMonth = identifyDateYearAndMonth($queryParamsDateYear, $queryParamsDateMonth);
-
-    $dateTimeMax = new DateTime(''.$dateYearAndMonth.'-01');
-    return $dateTimeMax->format('Y-m-t\T\2\3:\5\9:\5\9\Z');
-}*/
 
 function convertEventsToHtml($eventsResult) {
     $events = $eventsResult['items'];
