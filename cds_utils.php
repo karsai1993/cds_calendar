c723f1da24eb3a2e8434e9ecaa5db723e465764d
Karsai, Laszlo <karsai1993@gmail.com>
2023-03-13 20:37:11 +0100

    Fixed regex

--- cds_utils.php
+++ cds_utils.php
@@ -1,6 +1,7 @@
 <?php
 
 $lineBreakUrlEncoded = urlencode('<br>');
+$equalsUrlEncoded = urlencode('=');
 
 function resolveHtmlAsText($text, $length, $startPoint = 0) {
     $text = html_entity_decode(htmlspecialchars_decode($text));
@@ -18,10 +19,10 @@ function resolveDescriptionExtraInfo($extraInfo, $key) {
     $extraInfoParts = explode($lineBreakUrlEncoded, urlencode($extraInfo));
 
     foreach ($extraInfoParts as $extraInfoPart) {
-        $decodedExtraInfoPart = urldecode($extraInfoPart);
-        if (str_starts_with($decodedExtraInfoPart, $key)) {
-            $separatorIndex = strpos($decodedExtraInfoPart, '=');
-            return substr($decodedExtraInfoPart, $separatorIndex + 1);
+        if (str_starts_with($extraInfoPart, $key)) {
+            global $equalsUrlEncoded;
+            $separatorIndex = strpos($extraInfoPart, $equalsUrlEncoded);
+            return urldecode(substr($extraInfoPart, $separatorIndex + strlen($equalsUrlEncoded)));
         }
     }
 
@@ -52,11 +53,9 @@ function resolveDescriptionData($originalDescription) {
         $descriptionData['eventCategories'] = explode(',', $categoriesExtraInfo);
 
         $titleUrlExtraInfo = resolveDescriptionExtraInfo($extraInfo, 'title_url');
-        $descriptionData['eventTitleUrl'] = str_starts_with($titleUrlExtraInfo, '<a')
-            ?
-                (string)(new SimpleXMLElement($titleUrlExtraInfo))['href']
-            :
-                $titleUrlExtraInfo;
+        preg_match_all('~<a(.*?)href="([^"]+)"(.*?)>~', $titleUrlExtraInfo, $matches);
+        $matchedValues = $matches[2];
+        $descriptionData['eventTitleUrl'] = empty($matchedValues) ? $titleUrlExtraInfo : $matchedValues[0];
     }
 
     return $descriptionData;
